caa72042e42012e38eb6f4f07e2fa70dcfd0b435
Michi Saito <ms20054869tv@gmail.com>
2025-10-10 04:38:24 +0900

    db

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Middleware\TrustHosts as Middleware;
 class TrustHosts extends Middleware {
     protected function hosts(): array {
         return [
-            "15-168-43-164\\.sslip\\.io",
-            "15\\.168\\.43\\.164",
+            '15-168-43-164\.sslip\.io',
+            '15\.168\.43\.164',
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }
